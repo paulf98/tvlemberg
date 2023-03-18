86124381d28936099060735cadf631aba8dc02f3
Paul Faass <paul.faass@pxio.de>
2023-03-18 10:19:58 +0100

    feat(legal): add imprint

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 
 export default function Footer() {
 	return (
 		<footer className='footer footer-center p-10 bg-primary text-base-100'>
 			<div className='grid grid-flow-col gap-4 text-lg'>
-				<a className='link link-hover'>Impressum</a>
+				<Link className='link link-hover' href={'/impressum'}>
+					Impressum
+				</Link>
 				<a className='link link-hover'>Datenschutz</a>
 				<a className='link link-hover'>Satzung</a>
 				<a className='link link-hover'>Presse & Zeitung</a>
@@ -21,10 +24,11 @@ export default function Footer() {
 						<path d='M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z'></path>
 					</svg>
 				</a>
-				
 			</div>
 			<div>
-				<p className='italic'>Copyright © {new Date().getFullYear()} - Turnverein Lemberg e. V.</p>
+				<p className='italic'>
+					Copyright © {new Date().getFullYear()} - Turnverein Lemberg e. V.
+				</p>
 			</div>
 		</footer>
 	);
