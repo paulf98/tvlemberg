83ba620afcb863f92e8fbe956cd1636219c320fd
Paul Faass <paul.faass@pxio.de>
2023-03-18 15:02:35 +0100

    feat(page): add past results table

--- src/pages/veranstaltungen/laufspass.tsx
+++ src/pages/veranstaltungen/laufspass.tsx
@@ -2,6 +2,41 @@ import Image from 'next/image';
 import Laufspass1 from '../../../public/images/laufspass1.jpg';
 import Laufspass2 from '../../../public/images/laufspass2.jpg';
 
+const PastResults = [
+	{
+		year: 2020,
+		link: 'https://my.raceresult.com/149621/results?lang=de',
+	},
+	{
+		year: 2019,
+		link: 'https://my2.raceresult.com/96922/results?lang=de',
+	},
+	{
+		year: 2018,
+		link: 'http://my5.raceresult.com/96921/results?lang=de',
+	},
+	{
+		year: 2017,
+		link: 'http://my5.raceresult.com/54003/results?lang=de',
+	},
+	{
+		year: 2016,
+		link: 'http://my5.raceresult.com/48170/results?lang=de',
+	},
+	{
+		year: 2015,
+		link: 'http://my5.raceresult.com/34850/results?lang=de',
+	},
+	{
+		year: 2014,
+		link: 'http://my5.raceresult.com/24950/results?lang=de',
+	},
+	{
+		year: 2013,
+		link: 'http://my5.raceresult.com/15047/?lang=de',
+	},
+];
+
 export default function Laufspass() {
 	return (
 		<div className='text-center prose mx-auto'>
@@ -28,6 +63,34 @@ export default function Laufspass() {
 				alt='Laufspass'
 				className='mx-auto my-4 shadow-md rounded-md border'
 			/>
+			<h1 className='my-8'>Ergebnisse der vorherigen Läufe</h1>
+			<div className='overflow-x-auto'>
+				<table className='table w-full'>
+					{/* head */}
+					<thead>
+						<tr>
+							<th>Jahr</th>
+							<th>Link</th>
+						</tr>
+					</thead>
+					<tbody>
+						{PastResults.map((result) => (
+							<tr key={result.year}>
+								<th>{result.year}</th>
+								<td>
+									<a
+										className='link link-primary'
+										href={result.link}
+										target={'_blank'}
+									>
+										Zu RaceResult
+									</a>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
 		</div>
 	);
 }
