f3b092c5b6c22e4f8eed25d5518a586d6470ef64
Paul Faass <paul.faass@pxio.de>
2023-03-18 14:51:04 +0100

    feat(layout): adjust layout on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,31 +15,38 @@ export default function Home() {
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<h1 className='text-3xl font-bold text-center mb-8'>Neuigkeiten</h1>
-			<div className='flex flex-wrap justify-around items-start gap-4'>
-				<div className='card card-side bg-base-100 shadow-xl max-w-lg'>
-					<figure>
-						<Image src={TVLPlakat} alt='Veranstaltungsplakat' width={300} />
-					</figure>
-					<div className='card-body'>
-						<h2 className='card-title'>Comedy Dinner</h2>
-						<p>Am 22.04. in unserer Turnhalle.</p>
+			<div className='prose mx-auto'>
+				<h1 className='text-center mb-8'>Neuigkeiten</h1>
+				<div className='flex flex-col items-center justify-center gap-8'>
+					<div className='card card-side bg-base-100 shadow-xl w-100'>
+						<Image
+							src={TVLPlakat}
+							alt='Veranstaltungsplakat'
+							className='rounded-md object-cover m-0 max-w-xs'
+						/>
+						<div className='card-body'>
+							<h2 className='card-title'>Comedy Dinner</h2>
+							<p>Am 22.04. in unserer Turnhalle.</p>
+						</div>
 					</div>
-				</div>
 
-				<div className='card w-96 bg-base-100 shadow-xl'>
-					<div className='card-body'>
-						<h2 className='card-title'>Vereinscheck</h2>
-						<p>
-							Seit dem 24. Januar stellt der DOSB im Auftrag der Bundesregierung
-							bundesweit 150.000 Sportvereinsschecks á 40€ zur Verfügung. Wir
-							beteiligen uns gerne an dieser Aktion und nehmen ab sofort Eure
-							Schecks entgegen!
-						</p>
+					<div className='card bg-base-100 shadow-xl w-100'>
+						<div className='card-body'>
+							<h2 className='card-title'>Vereinscheck</h2>
+							<p>
+								Seit dem 24. Januar stellt der DOSB im Auftrag der
+								Bundesregierung bundesweit 150.000 Sportvereinsschecks á 40€ zur
+								Verfügung. Wir beteiligen uns gerne an dieser Aktion und nehmen
+								ab sofort Eure Schecks entgegen!
+							</p>
+						</div>
+
+						<Image
+							src={Vereinscheck}
+							alt='Vereinscheck'
+							className='rounded-md object-cover m-0'
+						/>
 					</div>
-					<figure>
-						<Image src={Vereinscheck} alt='Vereinscheck' />
-					</figure>
 				</div>
 			</div>
 		</>
