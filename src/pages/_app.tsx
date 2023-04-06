dd0a3e1e3cf09afffd2fea3fafaabca4234460e9
Paul Faass <paul.faass@pxio.de>
2023-04-06 22:57:08 +0200

    feat(vercel): add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Raleway } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 
 const font = Raleway({ subsets: ['latin'] });
 
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
 				}
 			`}</style>
 			<Component {...pageProps} />
+			<Analytics />
 		</>
 	);
 }
