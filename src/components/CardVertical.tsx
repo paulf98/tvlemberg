6cb441317be6a46658e6b4c30d16409e38f29423
Paul Faass <paulfaass@gmail.com>
2023-09-19 12:22:53 +0200

    fix(card-vertical): add link text prop and change max image size

--- src/components/CardVertical.tsx
+++ src/components/CardVertical.tsx
@@ -5,6 +5,7 @@ export type CardVerticalProps = {
 	text: React.ReactNode;
 	image: StaticImageData;
 	link?: string;
+	linkText?: string;
 };
 
 export default function CardVertical({
@@ -12,15 +13,16 @@ export default function CardVertical({
 	text,
 	image,
 	link,
+	linkText,
 }: CardVerticalProps) {
 	return (
 		<div className='card lg:card-side bg-base-100 shadow-xl'>
 			<Image
 				src={image.src}
 				alt={title}
-                width={image.width}
-                height={image.height}
-				className='rounded-md object-contain m-0 max-w-xs md:max-w-sm mx-auto'
+				width={image.width}
+				height={image.height}
+				className='rounded-md object-contain m-0 max-w-xs mx-auto'
 				sizes='(max-width: 640px) 100vw, 640px'
 				loading='lazy'
 			/>
@@ -32,9 +34,8 @@ export default function CardVertical({
 						className='btn btn-primary'
 						href={link}
 						target='_blank'
-						rel='noreferrer'
-					>
-						Zur Aktion
+						rel='noreferrer'>
+						{linkText || 'Zur Aktion'}
 					</a>
 				)}
 			</div>
