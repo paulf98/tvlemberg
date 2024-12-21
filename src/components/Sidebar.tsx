1483c2f835e40caf77b40ab322ef3c09b73611f2
Paul Faass <paulfaass@gmail.com>
2024-12-21 14:46:21 +0100

    chore: remove events 2024 from sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,6 @@ import Yogakurs from '../../public/images/yogakurs.jpg';
 import DAK from '../../public/images/dak.png';
 import Theyson from '../../public/images/logo_theysohn.svg';
 import Faass from '../../public/images/faass.png';
-import Termine from '../../public/images/Termine24.jpg';
 import Termine25 from '../../public/images/Termine25.jpg';
 
 /**
@@ -14,13 +13,6 @@ export default function Sidebar() {
 	return (
 		<aside className='hidden lg:flex flex-col gap-8 w-min-32 max-w-xs min-h-screen p-8'>
 			<h1 className='text-3xl font-bold text-center'>Aktuelles</h1>
-			<a href={Termine.src} target='_blank' className='hover:bg-base-200'>
-				<Image
-					className='shadow-xl rounded-md hover:bg-base-200'
-					src={Termine}
-					alt='Termine 2024'
-				/>
-			</a>
 			<a href={Termine25.src} target='_blank' className='hover:bg-base-200'>
 				<Image
 					className='shadow-xl rounded-md hover:bg-base-200'
