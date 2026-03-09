0b7e61e0b05cd5101c38edf22c0101b7ffa7d69b
Paul Faass <paulfaass@gmail.com>
2026-03-09 15:54:36 +0100

    fix: update laufspass

--- src/pages/veranstaltungen/laufspass.tsx
+++ src/pages/veranstaltungen/laufspass.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image';
-import Laufspass1 from '../../../public/images/Laufspass1.jpg';
+import Laufspass1 from '../../../public/images/Laufspass.jpg';
 
 const PastResults = [
+	{
+		year: 2025,
+		link: 'https://my.raceresult.com/318709/results',
+	},
 	{
 		year: 2024,
 		link: 'https://my.raceresult.com/281995/results',
@@ -49,18 +53,16 @@ export default function Laufspass() {
 		<div className='text-center prose mx-auto'>
 			<h1 className='mb-8'>Willkommen beim Lemberger Laufspass!</h1>
 			<p>
-				Der 11. Lemberger Laufspass findet erstmals im Rahmen des Laufladen-Cups
-				und der WasgauCup-Laufserie statt. Der TV Lemberg würde Dich gerne am
-				<strong> 11.05.2025</strong> begrüßen und wünscht Dir eine erfolgreiche
-				Teilnahme. Zur gezielten Vorbereitung bietet der Turnverein Lemberg zwei
-				drei Trainingsläufe unter der Leitung von Volker Ringelspacher und
-				Nathan Schmitt an. Die Termine dazu werden noch veröffentlicht.
-				Donnerstag 24.04 (18 Uhr) - Sonntag 27.04. (9.30 Uhr) - Montag 28.04 (18
-				Uhr) Treffpunkt ist jeweils der Parkplatz unterhalb der Burgruine
-				Lemberg.
+				Der TV 1891 Lemberg würde Dich gerne am Samstag <strong>09.05.2026</strong> beim 12.
+				Lemberger Laufspass begrüßen. Der Hauptlauf über 10,2 km und 210 hm startet um 16 Uhr. Zur
+				gezielten Vorbereitung bietet der Turnverein nachfolgende Trainingsläufe unter der Leitung
+				von Volker Ringelspacher und Nathan Schmitt an. Donnerstag 23.04 (18 Uhr) - Montag 27.04 (18
+				Uhr) - Donnerstag 30.04 (18 Uhr) - Sonntag 03.05 (10 Uhr). Treffpunkt ist jeweils der
+				Parkplatz unterhalb der Burgruine Lemberg. Der Turnverein wünscht Dir eine erfolgreiche
+				Vorbereitung und Teilnahme.
 			</p>
 			<a
-				href={'https://my.raceresult.com/318709/registration'}
+				href={'https://my.raceresult.com/387155/registration'}
 				target='_blank'
 				className='btn btn-primary mb-8'>
 				Zur Anmeldung bei Raceresult
@@ -70,7 +72,7 @@ export default function Laufspass() {
 				alt='Laufspass'
 				className='mx-auto my-4 shadow-md rounded-md border'
 			/>
-			<a download className='btn btn-primary' href='/assets/Laufspass2025.pdf'>
+			<a download className='btn btn-primary' href='/assets/Laufspass.pdf'>
 				Download als PDF
 			</a>
 			<h1 className='my-8'>Ergebnisse der vorherigen Läufe</h1>
@@ -88,10 +90,7 @@ export default function Laufspass() {
 							<tr key={result.year}>
 								<th>{result.year}</th>
 								<td>
-									<a
-										className='link link-primary'
-										href={result.link}
-										target={'_blank'}>
+									<a className='link link-primary' href={result.link} target={'_blank'}>
 										Zu RaceResult
 									</a>
 								</td>
